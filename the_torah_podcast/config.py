9816f092d385ad50be8e6111d799139ad7db0a9b
jeremyatia <spotep@gmail.com>
2024-03-21 13:09:39 +0200

    move subscribers.csv to thetorahpodcast repo

--- the_torah_podcast/config.py
+++ the_torah_podcast/config.py
@@ -1,3 +1,4 @@
 import os 
 
 path_to_file = os.path.dirname(os.path.abspath(__file__))
+link_github_subscribers = 'https://raw.githubusercontent.com/thetorahpodcast/youtube-to-anchorfm/main/subscribers.csv'
--- the_torah_podcast/main.py
+++ the_torah_podcast/main.py
@@ -1,12 +1,10 @@
-import os
-
 from upload_ids_to_repo import UploadIDstoRepo
-from config import path_to_file
 import pandas as pd
+from config import link_github_subscribers
 
 
 def run():
-    subscribers = pd.read_csv(os.path.join(path_to_file, 'subscribers.csv'))
+    subscribers = pd.read_csv(link_github_subscribers)
     for i in range(subscribers.shape[0]):
         channel_id, frequency_to_check, episode_name = subscribers.iloc[i]
         frequency_to_check = 1800
